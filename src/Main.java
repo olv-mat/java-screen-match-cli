6bf56c7bb051aee1669c03f9126fea0642c0906a
Matheus Oliveira <contamatheusprofissional@gmail.com>
2025-07-19 10:05:25 -0300

    refactor: adjusting interface items

--- src/Main.java
+++ src/Main.java
@@ -2,67 +2,65 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-
-        System.out.println("WELCOME TO SCREEN MATCH");
+        System.out.println("Screen Match");
         Scanner input = new Scanner(System.in);
         while (true) {
             String menu = """
-                    
-                    MENU
-                    1 - SEE OUR MOVIES CATALOG
-                    2 - RECOMMEND YOUR FAVORITE MOVIE
-                    3 - EXIT
+                    Menu
+                    1 - Movies Catalog
+                    2 - Add Movie
+                    3 - Exit
                     """;
-            System.out.println(menu);
-            System.out.print("YOUR OPTION: ");
+            System.out.println("=".repeat(30));
+            System.out.print(menu);
+            System.out.println("=".repeat(30));
+            System.out.print("Option: ");
             int option = input.nextInt();
             input.nextLine();
             switch (option) {
                 case 1:
                     boolean hasSubscribed = true;
                     if (hasSubscribed) {
-                        System.out.println("MOVIES CATALOG");
-                        String movieTitle = "HEREDITARY";
+                        System.out.println("Movies Catalog");
+                        String movieTitle = "Hereditary";
                         int releaseYear = 2018;
-                        String movieTag = releaseYear >= 2024 ? "NEW!" : "CLASSIC!";
-                        double averageRating = (9.8 + 6.3 + 8) / 3;
+                        String movieTag = releaseYear >= 2024 ? "New!" : "Classic!";
+                        double averageRating = (9.8 + 7.2 + 8 + 10) / 4;
                         int stars = (int) (averageRating / 2);
-                        System.out.println("------------------------------");
+                        System.out.println("=".repeat(30));
                         System.out.println(movieTag);
-                        System.out.println("MOVIE: " + movieTitle);
-                        System.out.println("RELEASE YEAR: " + releaseYear);
-                        System.out.println("AVERAGE RATING: " + averageRating);
-                        System.out.println("STARS: " + stars);
-                        System.out.println("------------------------------");
+                        System.out.println("Movie: " + movieTitle);
+                        System.out.println("Release Year: " + releaseYear);
+                        System.out.println("Average Rating: " + averageRating);
+                        System.out.println("Stars: " + stars);
                     } else {
-                        System.out.println("PLEASE, SUBSCRIBE TO OUR PLATFORM");
+                        System.out.println("Please, Subscribe To Our Platform");
                     }
                     break;
                 case 2:
-                    System.out.println("RECOMMEND YOUR FAVORITE MOVIE");
-                    System.out.println("------------------------------");
-                    System.out.print("ENTER YOUR FAVORITE MOVIE: ");
-                    String favoriteMovie = input.nextLine();
-                    System.out.print("ENTER THE RELEASE DATE OF YOUR FAVORITE MOVIE: ");
-                    int releaseYearFavoriteMovie = input.nextInt();
-                    double averageRatingFavoriteMovie = 0;
-                    for (int i = 0; i < 3; i++) {
-                        System.out.printf("ENTER RATING %d: ", i + 1);
-                        averageRatingFavoriteMovie += input.nextDouble();
+                    System.out.println("Add Movie");
+                    System.out.println("=".repeat(30));
+                    System.out.print("Movie Title: ");
+                    String newMovieTitle = input.nextLine();
+                    System.out.print("Release Year: ");
+                    int newMovieReleaseYear = input.nextInt();
+                    double newMovieAverageRating = 0;
+                    for (int i = 0; i < 4; i++) {
+                        System.out.printf("Rating %d: ", i + 1);
+                        newMovieAverageRating += input.nextDouble();
                     }
-                    averageRatingFavoriteMovie /= 3;
-                    System.out.println("------------------------------");
-                    System.out.printf("FAVORITE MOVIE: %s%n", favoriteMovie);
-                    System.out.printf("RELEASE YEAR: %d%n", releaseYearFavoriteMovie );
-                    System.out.printf("AVERAGE RATING: %f%n", averageRatingFavoriteMovie);
-                    System.out.println("THANKS FOR THE RECOMMENDATION");
-                    System.out.println("------------------------------");
+                    newMovieAverageRating /= 4;
+                    System.out.println("=".repeat(30));
+                    System.out.println("New Movie Added Successfully");
+                    System.out.printf("Movie: %s%n", newMovieTitle);
+                    System.out.printf("Release Year: %d%n", newMovieReleaseYear);
+                    System.out.printf("Average Rating: %f%n", newMovieAverageRating);
                     break;
                 case 3:
-                    System.out.println("THANK YOU FOR USING SCREEN MATCH. SEE YOU NEXT TIME!");
+                    System.out.println("Thank You For Using Screen Match. See You Next Time!");
                     return;
                 default:
-                    System.out.println("INVALID OPTION");
+                    System.out.println("Invalid Option");
                     break;
             }
         }
